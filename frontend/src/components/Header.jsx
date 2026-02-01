462bfcc71e55169d52bb7eb8c68ee67ab2232c06
Thomas Woroniak <thomasworoniak@gmail.com>
2026-02-01 15:15:04 -0600

    Add custom Bootstrap css

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import logo from '../assets/logo.png';
 
 const Header = () => {
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>ProShop</Navbar.Brand>
+          <Navbar.Brand href='/'>
+            <img src={logo} alt='ProShop logo' />
+            ProShop
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
               <Nav.Link href='/cart'>
-                <FaShoppingCart />
-              </Nav.Link>{' '}
-              Cart
+                <FaShoppingCart /> Cart
+              </Nav.Link>
+
               <Nav.Link href='/login'>
-                <FaUser />
-              </Nav.Link>{' '}
-              Sign In
+                <FaUser /> Sign In
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
