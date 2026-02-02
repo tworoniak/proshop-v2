9cb62d9b529c5fc51afc27376c4aba6105c8e0ee
Thomas Woroniak <thomasworoniak@gmail.com>
2026-02-01 20:05:19 -0600

    Modeling data

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
-import express from 'express';
-import products from './data/products.js';
 import dotenv from 'dotenv';
 dotenv.config();
-const port = process.env.PORT;
+
+import express from 'express';
+import products from './data/products.js';
+import connectDB from './config/db.js';
+
+connectDB();
 
 const app = express();
+const port = process.env.PORT || 5000;
 
 app.get('/api/products', (req, res) => {
   res.json(products);
@@ -12,8 +16,9 @@ app.get('/api/products', (req, res) => {
 
 app.get('/api/products/:id', (req, res) => {
   const product = products.find((p) => p._id === req.params.id);
-
   res.json(product);
 });
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
