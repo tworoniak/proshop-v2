ab7364ee3b971236111b33e62139d3d18cc6ac88
Thomas Woroniak <thomasworoniak@gmail.com>
2026-02-02 14:12:35 -0600

    add middleware, fetch from database, add product controller

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,2 +1,11 @@
 import express from 'express';
 const router = express.Router();
+import {
+  getProducts,
+  getProductById,
+} from '../controllers/productController.js';
+
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
+
+export default router;
