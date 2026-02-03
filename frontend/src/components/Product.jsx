101b63c67100ea424e359de6748786bd1969a9f1
Thomas Woroniak <thomasworoniak@gmail.com>
2026-02-02 20:36:15 -0600

    add shopping cart screen and functionality

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -23,7 +23,7 @@ const Product = ({ product }) => {
         <Card.Text as='div'>
           <Rating
             value={product.rating}
-            text={`${product.reviews.length} reviews`}
+            text={`${product.numReviews} reviews`}
           />
         </Card.Text>
         <Card.Text as='h3'>${product.price}</Card.Text>
